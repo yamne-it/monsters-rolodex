9b363d2dbce4894d5575abfe6466595f4fd528df
Damiano <damiano.callegari@gmail.com>
2020-07-06 19:02:55 +0200

    Updates

---
+++ precache-manifest.bae22dd16553091d1e5d6ae0021d19d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba576d340c1ae57520d5af7d906d6cb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bee09751c7b981a5d983",
+    "url": "/monsters-rolodex/static/css/main.250a8e48.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee09751c7b981a5d983",
+    "url": "/monsters-rolodex/static/js/main.2945140e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
